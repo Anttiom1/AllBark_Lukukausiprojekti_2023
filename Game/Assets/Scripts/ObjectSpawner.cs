ffc47b45f260b2c8d4ab3802b71b2de0fcfa0d35
Antti Mursu <Antti.Mursu@edu.lapinamk.fi>
2023-10-17 15:55:39 +0300

    added stones to spawner

--- Game/Assets/Scripts/ObjectSpawner.cs
+++ Game/Assets/Scripts/ObjectSpawner.cs
@@ -12,9 +12,16 @@ public class ObjectSpawner : MonoBehaviour
         while (amountOfTrees < 25) {
             int randomIndex = Random.Range(0, myObjects.Length);
             Vector3 randomSpawnPoint = new Vector3(Random.Range(-20, 20), 2.3f, (Random.Range(-20, 20)));
-
-            Instantiate(myObjects[randomIndex], randomSpawnPoint, Quaternion.identity);
+            Instantiate(myObjects[0], randomSpawnPoint, Quaternion.identity);
             amountOfTrees++;
+        }
+        int amountOfStones = 0;
+        while (amountOfStones < 10)
+        {
+            int randomIndex = Random.Range(0, myObjects.Length);
+            Vector3 randomSpawnPoint = new Vector3(Random.Range(-20, 20), 0, (Random.Range(-20, 20)));
+            Instantiate(myObjects[1], randomSpawnPoint, Quaternion.identity);
+            amountOfStones++;
         }
     }
 

1d19ba74bcefa0fbb75a3409af4d5fc12dde3518
Anttiom1 <antti-mursu@hotmail.com>
2023-11-22 15:31:53 +0200

    Controller

--- Controller/BLE_Controller/BLE_Controller.ino
+++ Controller/BLE_Controller/BLE_Controller.ino
@@ -2,6 +2,9 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 #include <BLE2902.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
@@ -9,7 +12,7 @@
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 #define CHARACTERISTIC_UUIDa "617c753e-5199-11eb-ae93-0242ac130002"
 
-
+Adafruit_MPU6050 mpu;
 bool deviceConnected = false;
 class MyServerCallbacks : public BLEServerCallbacks {
   void onConnect(BLEServer *pServer) {
@@ -27,12 +30,67 @@ BLECharacteristic *pCharacteristica;
 
 // constants won't change
 #define LIGHT_SENSOR_PIN 36  // ESP32 pin GPIO36 (ADC0) connected to light sensor
-#define LED_PIN 17           // 
+#define LED_PIN 17           //
 #define ANALOG_THRESHOLD 1500
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);  // set ESP32 pin to output mode
   Serial.begin(9600);
+
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+    case MPU6050_RANGE_250_DEG:
+      Serial.println("+- 250 deg/s");
+      break;
+    case MPU6050_RANGE_500_DEG:
+      Serial.println("+- 500 deg/s");
+      break;
+    case MPU6050_RANGE_1000_DEG:
+      Serial.println("+- 1000 deg/s");
+      break;
+    case MPU6050_RANGE_2000_DEG:
+      Serial.println("+- 2000 deg/s");
+      break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+    case MPU6050_BAND_260_HZ:
+      Serial.println("260 Hz");
+      break;
+    case MPU6050_BAND_184_HZ:
+      Serial.println("184 Hz");
+      break;
+    case MPU6050_BAND_94_HZ:
+      Serial.println("94 Hz");
+      break;
+    case MPU6050_BAND_44_HZ:
+      Serial.println("44 Hz");
+      break;
+    case MPU6050_BAND_21_HZ:
+      Serial.println("21 Hz");
+      break;
+    case MPU6050_BAND_10_HZ:
+      Serial.println("10 Hz");
+      break;
+    case MPU6050_BAND_5_HZ:
+      Serial.println("5 Hz");
+      break;
+
+
+  }
+
+
 
   BLEDevice::init("AllBark");
   BLEServer *pServer = BLEDevice::createServer();
@@ -56,22 +114,51 @@ void setup() {
 
 void loop() {
 
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  //Serial.print("Rotation X: ");
+  //Serial.print(g.gyro.x);
+  //Serial.print(", Y: ");
+  //Serial.print(g.gyro.y);
+  //Serial.print(", Z: ");
+  //Serial.print(g.gyro.z);
+  //Serial.println(" rad/s");
+  int i = 0;
+  int x = g.gyro.x * 10;
+  int y = g.gyro.y * 10;
+  int z = g.gyro.z * 10;
+  //Serial.println(x);
+  i = i<<8;
+  i |= x;
+  i = i<<8;
+  i |= y;
+  i = i<<8;
+  i |= z;
+  Serial.print("X:");
+  Serial.println(x);
+  Serial.print("Y:");
+  Serial.println(y);
+  Serial.print("Z:");
+  Serial.println(z);
+  Serial.print("i BIN: ");
+  Serial.println(i, BIN);
+  delay(100);
+
+
   int analogValue = analogRead(LIGHT_SENSOR_PIN);  // read the value on analog pin
-  //digitalWrite(LED_PIN, HIGH); // turn on LED
+  digitalWrite(LED_PIN, HIGH);                     // turn on LED
   //Serial.println(analogValue);
   if (deviceConnected) {
-    Serial.println("Device Connected");
-    if (analogValue == 0) {
-      int i = 0b0001;
-      Serial.println(i);
-      pCharacteristic->setValue(i);
-      pCharacteristic->notify();
-    }
-    if (analogValue > 1) {
-      int i = 0b0010;
-      Serial.println(i);
-      pCharacteristic->setValue(i);
-      pCharacteristic->notify();
-    }
+    //Serial.println("Device Connected");
+    
+
+
+    //if (analogValue > 1500) {
+    //int i = 0b0010;
+    //Serial.println(i);
+    //pCharacteristic->setValue(i);
+    //pCharacteristic->notify();
   }
 }
+
---
+++ Controller/gyrokoodi2_31_10_2023/gyrokoodi2_31_10_2023.ino
@@ -0,0 +1,206 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+#if defined(ESP8266)
+#endif
+
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define CHARACTERISTIC_UUIDa "617c753e-5199-11eb-ae93-0242ac130002"
+
+MPU6050 mpu;
+
+bool deviceConnected = false;
+class MyServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer *pServer) {
+    deviceConnected = true;
+  };
+
+  void onDisconnect(BLEServer *pServer) {
+    deviceConnected = false;
+  }
+};
+
+BLECharacteristic *pCharacteristic;
+BLECharacteristic *pCharacteristica;
+
+// constants won't change
+#define LIGHT_SENSOR_PIN 36  // ESP32 pin GPIO36 (ADC0) connected to light sensor
+#define LED_PIN 17           //
+#define ANALOG_THRESHOLD 1500
+#define INTERRUPT_PIN 39
+
+// MPU control/status vars
+bool dmpReady = false;   // set true if DMP init was successful
+uint8_t mpuIntStatus;    // holds actual interrupt status byte from MPU
+uint8_t devStatus;       // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;     // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;      // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64];  // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;         // [w, x, y, z]         quaternion container
+VectorInt16 aa;       // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;   // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;  // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;  // [x, y, z]            gravity vector
+
+
+// uncomment "OUTPUT_READABLE_QUATERNION" if you want to see the actual
+// quaternion components in a [w, x, y, z] format (not best for parsing
+// on a remote host such as Processing or something though)
+#define OUTPUT_READABLE_QUATERNION
+
+const char DEVICE_NAME[] = "mpu6050";
+
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;  // indicates whether MPU interrupt pin has gone high
+void ICACHE_RAM_ATTR dmpDataReady() {
+  mpuInterrupt = true;
+}
+
+void mpu_setup() {
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+  Wire.setClock(400000);  // 400kHz I2C clock. Comment this line if having compilation difficulties
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
+
+  // initialize device
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+
+  // verify connection
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788);  // 1688 factory default for my test chip
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
+}
+
+void setup(void) {
+  Serial.begin(115200);
+  mpu_setup();
+  
+  BLEDevice::init("AllBark");
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  pCharacteristic = pService->createCharacteristic(
+    CHARACTERISTIC_UUID,
+    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_INDICATE);
+
+  pCharacteristica = pService->createCharacteristic(
+    CHARACTERISTIC_UUIDa,
+    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_INDICATE);
+
+  pCharacteristica->addDescriptor(new BLE2902());
+
+  pCharacteristic->addDescriptor(new BLE2902());
+  pService->start();
+  pServer->getAdvertising()->start();
+
+}
+
+void mpu_loop() {
+  // if programming failed, don't try to do anything
+  if (!dmpReady) return;
+
+  // wait for MPU interrupt or extra packet(s) available
+  if (!mpuInterrupt && fifoCount < packetSize) return;
+
+  // reset interrupt flag and get INT_STATUS byte
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+
+  // get current FIFO count
+  fifoCount = mpu.getFIFOCount();
+
+  // check for overflow (this should never happen unless our code is too inefficient)
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    // reset so we can continue cleanly
+    mpu.resetFIFO();
+    Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+  } else if (mpuIntStatus & 0x02) {
+    // wait for correct available data length, should be a VERY short wait
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+    // read a packet from FIFO
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+    // track FIFO count here in case there is > 1 packet available
+    // (this lets us immediately read more without waiting for an interrupt)
+    fifoCount -= packetSize;
+
+#ifdef OUTPUT_READABLE_QUATERNION
+    // display quaternion values in easy matrix form: w x y z
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    int i = 0;
+    int x = q.x * 100;
+    i |= x;
+    i = i<<8;
+    Serial.print("x: ");
+    Serial.println(x);
+    Serial.print("i BIN: ");
+    Serial.println(i, BIN);
+#endif
+  }
+}
+
+
+void loop(void) {
+  if (deviceConnected) {
+    Serial.println("Device Connected");
+  }
+  mpu_loop();
+}

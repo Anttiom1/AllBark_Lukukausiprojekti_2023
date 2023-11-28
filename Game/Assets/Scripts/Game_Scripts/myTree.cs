b751907b5b2da54fb88dc24d55712b18b53a3b4a
aksaloma <aksaloma@lapinamk.fi>
2023-11-28 14:45:38 +0200

    myTree now works with soundeffects, added placeholder sound

--- Game/Assets/Scripts/Game_Scripts/myTree.cs
+++ Game/Assets/Scripts/Game_Scripts/myTree.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 public class myTree : MonoBehaviour, IObjectManager
 {
     public GameObject Stump;
+    private AudioSource audioSource;
     private bool isFalling = false;
     // Tree falling speed
     private float fallingSpeed = 65f; 
     // Angle where tree -> stump
     private float maxFallingAngle = 45f; 
+
+    void Start()
+    {
+        // Get audio source
+        audioSource = GetComponent<AudioSource>();
+    }
 
     void Update()
     {
@@ -28,7 +35,15 @@ public class myTree : MonoBehaviour, IObjectManager
     public virtual float Collide()
     {
         isFalling = true;
+        PlaySound(); // Play sound effect
         return -10;
+    }
+        private void PlaySound()
+    {
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
     }
 
     private void CreateStump()

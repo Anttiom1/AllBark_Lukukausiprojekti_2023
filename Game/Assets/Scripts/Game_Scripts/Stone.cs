d548c0b52dba6aabc967c03e88cdb713a57bd95f
aksaloma <aksaloma@lapinamk.fi>
2023-11-28 12:40:31 +0200

    Made stone script work with sound effects, added a placeholder sound effect on rock collide

--- Game/Assets/Scripts/Game_Scripts/Stone.cs
+++ Game/Assets/Scripts/Game_Scripts/Stone.cs
@@ -1,9 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Stone : MonoBehaviour, IObjectManager
 {
+    private AudioSource audioSource; // Tässä määritellään audioSource
+
+    void Start()
+    {
+        // Tässä haetaan ja tallennetaan AudioSource-komponentti audioSource-muuttujaan
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public float Collide()
     {
         return 0;
@@ -12,7 +18,15 @@ public class Stone : MonoBehaviour, IObjectManager
     private void OnTriggerEnter(Collider other)
     {
         Player player = other.GetComponent<Player>();
-        player.Stop();
-    }
+        if (player != null)
+        {
+            player.Stop();
 
+            // Tarkista, onko audioSource määritelty ja toista ääni
+            if (audioSource != null)
+            {
+                audioSource.Play();
+            }
+        }
+    }
 }
